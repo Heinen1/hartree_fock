7db49459ec1ed201eee309399a1b517bfbe4c069
Heinen1 <jurnheinen111@gmail.com>
2023-01-02 21:51:31 +0100

    remove code duplicate

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ def main():
     # load xyz into molecule object and load basis functions automatically
     molecule = readxyz('water.xyz')
 
-    S = overlap_recursive(molecule.molecule_bf)
-    T = kinetic_recursive(molecule.molecule_bf)
-    VNe = nuclear_electron_recursive(molecule.molecule_bf,
-        [atom['coords'] for atom in molecule.atoms],
-        [atom['element'] for atom in molecule.atoms])
-
+    # store atom coordinates and elements in array
     atom_coords = np.array([atom['coords'] for atom in molecule.atoms])
     atom_elements = np.array([atom['element'] for atom in molecule.atoms])
-    VNN = nuclear_nuclear_repulsion_energy(atom_coords, atom_elements)
+
+    S = overlap_recursive(molecule.molecule_bf)
+    T = kinetic_recursive(molecule.molecule_bf)
+    VNe = nuclear_electron_recursive(
+        molecule.molecule_bf,
+        atom_coords,
+        atom_elements)
+    VNN = nuclear_nuclear_repulsion_energy(
+        atom_coords, 
+        atom_elements)
     Vee = electron_electron_repulsion(molecule.molecule_bf)
 
 if __name__ == '__main':
