1e093b99e372ac844f761ece3be624ab6731c55b
Heinen1 <jurnheinen111@gmail.com>
2023-01-02 09:42:31 +0100

    compare 1s and higher angular moment integrals

--- main_1s.py
+++ main_1s.py
@@ -1,9 +1,11 @@
 """Calculate integrals for 1s-orbitals only."""
+from time import perf_counter # for timing the calls to the functions
 import numpy as np
 from integrals_spherical import *  # integrals valid for 1s only
 from classes.c_primitive_gaussian import primitive_gaussian
 from scf import scf_cycle
 from integrals import *
+from classes.c_atom2 import atom2
 
 # STO-3G basis for 1s orbital on hydrogen
 # exponents and zeta taken from basissetexchange.org
@@ -19,13 +21,28 @@ H2_1s = [H2_pg1a, H2_pg1b, H2_pg1c]
 molecule = [H1_1s, H2_1s]
 atom_coordinates = np.array([[0, 0, 0], [0, 0, 1.4]])
 Z = [1.0, 1.0]
+
 print(overlap(molecule))
+print(overlap_recursive(molecule))
 print(overlap_numerical(molecule))
 print(kinetic(molecule))
+print(kinetic_recursive(molecule))
 print(electron_nuclear_attraction(molecule, atom_coordinates, Z)) # atomic units
 print(nuclear_electron_recursive(molecule, atom_coordinates, ['H', 'H']))
-print("Elec-Electron repulsion", electron_electron_repulsion(molecule))
+
+t1_start = perf_counter()
+print(electron_electron_repulsion(molecule))
+t1_end = perf_counter()
+
+t2_start = perf_counter()
+electron_electron_repulsion_s(molecule)
+t2_end = perf_counter()
+
+print("Recursive: ", t1_end - t1_start)
+print("Non-Recursive: ", t2_end - t2_start)
+
 print("Nuc-Nuc repulsion", nuclear_nuclear_repulsion_energy(atom_coordinates, Z))
+print("Nuc-Nuc repulsion", nuclear_nuclear_repulsion_energy(atom_coordinates, ['H', 'H']))
 
 
 # 6-31G basis for 1s orbital on hydrogen
@@ -75,7 +92,7 @@ for molecule_coordinate in molecule_coordinates:
     S = overlap(molecule)
     T = kinetic(molecule)
     Vne = electron_nuclear_attraction(molecule, atom_coordinates, [1.0, 1.0])
-    Vee = electron_electron_repulsion(molecule)
+    Vee = electron_electron_repulsion_s(molecule)
     Enn = nuclear_nuclear_repulsion_energy(atom_coordinates, Z)
     molecular_terms = [S, T, Vne, Vee]
     scf_parameters = [1e-5, 20, ]
@@ -91,3 +108,35 @@ import matplotlib.pyplot as plt
 plt.xlabel("Bond distance (A)")
 plt.ylabel("Total energy (Ha)")
 plt.plot(np.array(distances) * conversion, total_energies)
+
+
+# Use recursive integrals
+# create many H2 molecules in STO-3G basis
+distances = [round(i*0.1, 2) for i in range(5, 51)]  # unit = bohr (a.u. of position)
+molecule_coordinates = [ [[0, 0, 0], [0, 0, distance]] for distance in distances ]
+total_energies_rec = []
+
+for molecule_coordinate in molecule_coordinates:
+    H1a = atom2('H', molecule_coordinate[0])
+    H2a = atom2('H', molecule_coordinate[1])
+    molecule = H1a.bf + H2a.bf
+    atom_coordinates = np.array([molecule_coordinate[0], molecule_coordinate[1]])
+    Z = [1.0, 1.0]
+    # compute SCF energy
+    S = overlap_recursive(molecule)
+    T = kinetic_recursive(molecule)
+    Vne = nuclear_electron_recursive(molecule, atom_coordinates, ['H', 'H'])
+    Vee = electron_electron_repulsion(molecule)
+    Enn = nuclear_nuclear_repulsion_energy(atom_coordinates, ['H', 'H'])
+    molecular_terms = [S, T, Vne, Vee]
+    scf_parameters = [1e-5, 20, ]
+    electronic_energy = scf_cycle(molecular_terms, scf_parameters, molecule)
+    # compute total energy =SCF energy + E_NN
+    total_energy = electronic_energy + Enn
+    total_energies_rec.append(total_energy)
+
+import matplotlib.pyplot as plt
+plt.xlabel("Bond distance (A)")
+plt.ylabel("Total energy (Ha)")
+plt.plot(np.array(distances) * conversion, total_energies_rec)
+    
